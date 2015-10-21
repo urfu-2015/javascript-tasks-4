2e5789810e9ac1eb108bfbc6ad101f4985f2ee1e
Valeriyan <rebelling.valeriyan@gmail.com>
2015-10-21 18:17:42 +0500

    1

--- iterator.js
+++ iterator.js
@@ -1,4 +1,23 @@
 'use strict';
 
 module.exports.get = function (collection, startPoint, depth) {
+    var currentDepth = 1;
+    var currentPoint = startPoint;
+    var currentIndex = 0;
+
+    return {
+        next: function () {
+            var a = collection;
+            var b = currentPoint;
+            var add = a['Сергей'];
+            if (currentIndex < collection[currentPoint].friends.length) {
+                currentIndex++;
+                currentPoint = collection.currentPoint.firends[currentIndex];
+                return {name: currentPoint, phone: collection.currentPoint.phone};
+            }
+        },
+        nextMale: function() {
+
+        }
+    };
 };
