23ed45cc5ac111b42b8c5d237f2d32d27e6f26ef
sorovlad <fafafa@e1.ru>
2015-10-24 20:36:06 +0500

    основное + удаление

--- iterator.js
+++ iterator.js
@@ -1,4 +1,144 @@
 'use strict';
 
 module.exports.get = function (collection, startPoint, depth) {
+    return new Iterator(collection, startPoint, depth);
+};
+
+function Iterator(collection, startPoint, depth) {
+    var curent = 0;
+
+    depth = Number.isInteger(depth) ? depth : 0;
+
+    if (typeof startPoint === undefined || Object.keys(collection).indexOf(startPoint) === -1) {
+        curent = NaN;
+    } else {
+        var listNames = createListNames(startPoint);
+    }
+
+    function createListNames(name) {
+        var curentDepth = -1;
+        var following = [];
+        var curents = [name];
+        var result = [];
+
+        putNamesInResult();
+        return result;
+
+        function putNamesInResult() {
+            for (var name of curents) {
+                result.push(name);
+                following = following.concat(getFriends(name));
+            }
+            if (following.length === 0) {
+                return;
+            }
+
+            curents = following;
+            following = [];
+            curentDepth ++;
+
+            if (depth - curentDepth !== 0 || depth === 0) {
+                putNamesInResult();
+            }
+        }
+
+        function getFriends(name) {
+            var friends = [];
+            for (var friend of collection[name]['friends']) {
+                if (curents.indexOf(friend) === -1 &&
+                    following.indexOf(friend) === -1 &&
+                    result.indexOf(friend) === -1 &&
+                    Object.keys(collection).indexOf(friend) !== -1) {
+                    friends.push(friend);
+                }
+            }
+            return friends;
+        }
+    }
+
+    this.next = function () {
+        if (isNaN(curent)) {
+            return null;
+        }
+        isChange() || rebuild();
+
+        if (arguments.length > 0) {
+            var indexElement = listNames.indexOf(arguments[0]);
+            if (indexElement === -1) {
+                curent = listNames.length;
+                return null;
+            }
+            curent = indexElement;
+            return collection[listNames[indexElement]];
+        }
+
+        curent ++;
+        if (curent >= listNames.length) {
+            curent = listNames.length;
+            return null;
+        }
+
+        return collection[listNames[curent]];
+    };
+
+    this.prev = function () {
+        if (isNaN(curent)) {
+            return null;
+        }
+        isChange() || rebuild();
+
+        curent --;
+        if (curent < 0) {
+            curent = -1;
+            return null;
+        }
+
+        return collection[listNames[curent]];
+    };
+
+    this.nextMale = function () {
+        if (isNaN(curent)) {
+            return null;
+        }
+        isChange() || rebuild();
+
+        for (var i = curent + 1; i < listNames.length; i++) {
+            if (collection[listNames[i]]['gender'] === 'Мужской') {
+                curent = i;
+                return collection[listNames[i]];
+            }
+        };
+        curent = listNames.length;
+        return null;
+    };
+
+    this.prevMale = function () {
+        if (isNaN(curent)) {
+            return null;
+        }
+        isChange() || rebuild();
+
+        for (var i = curent - 1; i > -1; i--) {
+            if (collection[listNames[i]]['gender'] === 'Мужской') {
+                curent = i;
+                return collection[listNames[i]];
+            }
+        };
+
+        curent = -1;
+        return null;
+    };
+
+    function isChange() {
+        return listNames.every(item => {
+            return Object.keys(collection).indexOf(item) !== -1;
+        });
+    }
+    function rebuild() {
+        if (Object.keys(collection).indexOf(startPoint) === -1) {
+            curent = NaN;
+            return;
+        }
+        listNames = createListNames(startPoint);
+    }
 };
