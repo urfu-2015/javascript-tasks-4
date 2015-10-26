f27b1d558ce57db10e23ed6ab0e362e4db99bc34
Savi <Ultimate95@mail.ru>
2015-10-27 01:29:41 +0500

    fixes

--- iterator.js
+++ iterator.js
@@ -1,86 +1,108 @@
 'use strict';
 
 module.exports.get = function (collection, startPoint, depth) {
-	var result = [null];
-	var badVar;
-	var currentFriendIndex = 1;
-	var currentDudeIndex = 0;
-	if (!(startPoint in collection)) {
-		badVar = false;
-	} else {
-		var queue = [];
-		var used = [];
-		var d = {};
-		for (var el in collection) {
-			d[el] = 0;
-		}
-		queue.push(startPoint);
-		used.push(startPoint);
-		main: while (queue.length != 0) {
-			var currentFriend = queue.shift();
-			for (var i = 0; i < collection[currentFriend].friends.sort().length; i++) {
-				var hisFriend = collection[currentFriend].friends[i];
-				if (used.indexOf(hisFriend) == -1) {
-					d[hisFriend] = d[currentFriend] + 1;
-					if (d[hisFriend] > depth) {
-						break main;
-					}
-					used.push(hisFriend);
-					queue.push(hisFriend);
-					result.push(getOutput(hisFriend));
-				}
-			}
-		}
-	}
+    var result = [null];
+    var badVar;
+    var currentFriendIndex = 0;
+    if (depth === undefined) {
+        depth = Infinity;
+    }
+    if (!(startPoint in collection)) {
+        badVar = false;
+    } else {
+        var queue = [];
+        var used = [];
+        var d = {};
+        for (var el in collection) {
+            d[el] = 0;
+        }
+        queue.push(startPoint);
+        used.push(startPoint);
+        main: while (queue.length != 0) {
+            var currentFriend = queue.shift();
+            for (var i = 0; i < collection[currentFriend].friends.sort().length; i++) {
+                var hisFriend = collection[currentFriend].friends[i];
+                if (used.indexOf(hisFriend) == -1) {
+                    d[hisFriend] = d[currentFriend] + 1;
+                    if (d[hisFriend] > depth) {
+                        break main;
+                    }
+                    used.push(hisFriend);
+                    queue.push(hisFriend);
+                    result.push(getOutput(hisFriend));
+                }
+            }
+        }
+    }
+    console.log(result);
 
-	function getNext() {
-		if (!badVar) {
-			if (currentFriendIndex < result.length) {
-				console.log(result[currentFriendIndex]);
-				currentFriendIndex++;
-			} else {
-				console.log(null);
-			}
-		} else {
-			console.log(null);
-		}
-	};
+    function getNext(name) {
+        if (!badVar) {
+            currentFriendIndex++;
+            if (currentFriendIndex < result.length) {
+                if (name !== undefined) {
+                    while (result[currentFriendIndex].name != name) {
+                        currentFriendIndex++;
+                    }
+                }
+                return result[currentFriendIndex];
+            } else {
+                currentFriendIndex = result.length;
+                return null;
+            }
+        } else {
+            return null;
+        }
+    };
 
-	function getPrev() {
-		if ((result.length - currentFriendIndex) == 2) {
-			currentFriendIndex -= 2;
-		} else {
-			currentFriendIndex--;
-		}
-		if (currentFriendIndex <= 0) {
-			console.log(null);
-			currentFriendIndex = 1;
-		} else {
-			console.log(result[currentFriendIndex]);
-		}
-	};
+    function getPrev() {
+        currentFriendIndex--;
+        if (currentFriendIndex <= 0) {
+            currentFriendIndex = 1;
+            return null;
+        } else {
+            return result[currentFriendIndex];
+        }
+    };
 
-	function getOutput(name) {
-		var temp_result = {};
-		temp_result['name'] = name;
-		temp_result['phone'] = collection[name]['phone'];
-		return temp_result;
-	};
+    function getOutput(name) {
+        var temp_result = {};
+        temp_result['name'] = name;
+        temp_result['phone'] = collection[name]['phone'];
+        return temp_result;
+    };
 
-	// function searchNextMale() {
-	// 	while (collection[result[currentDudeIndex]['name']].gender != 'Мужской') {
-	// 		currentDudeIndex++;
-	// 	}
-	// 	return currentDudeIndex;
-	// };
+    function getNextMale() {
+        currentFriendIndex++;
+        if (currentFriendIndex < result.length) {
+            while (collection[result[currentFriendIndex]['name']].gender != 'Мужской') {
+                currentFriendIndex++;
+            }
+            return result[currentFriendIndex];
+        } else {
+            currentFriendIndex = result.length;
+            return null;
+        }
+    };
 
-	// function getNextMale() {
-	// 	console.log('dude', result[searchNextMale()]);
-	// };
+    function getPrevMale() {
+        currentFriendIndex--;
+        if (currentFriendIndex <= 0) {
+            currentFriendIndex = 1;
+            return null;
+        } else {
+            while (collection[result[currentFriendIndex]['name']].gender != 'Мужской') {
+                currentFriendIndex--;
+            }
+            console.log(currentFriendIndex);
+            return result[currentFriendIndex];
+        }
+    };
 
-	return {
-		next: getNext,
-		prev: getPrev
-		// nextMale: getNextMale
-	}
+    return {
+        next: getNext,
+        prev: getPrev,
+        nextMale: getNextMale,
+        prevMale: getPrevMale
+    };
 };
