ed6447a61e910230e8386ef0e3f7e14b676753ef
Fedor <mydearfrodo@yandex.ru>
2015-10-26 16:35:32 +0500

    feat(iterator): base code

--- iterator.js
+++ iterator.js
@@ -1,4 +1,130 @@
 'use strict';
+var MAXIMAL_DEPTH = 20;
+module.exports.get = function (collection, nameStart, maxDepth) {
+    var currentIndexFriend = 0;
+    if (maxDepth == undefined) {
+        maxDepth = MAXIMAL_DEPTH;
+    }
+    var friends = initFriends(collection, nameStart, maxDepth);
+    return {
+        next: function () {
+            var resultObject;
+            if (!arguments[0]) {
+                currentIndexFriend += 1;
+                if (currentIndexFriend >= friends.length) {
+                    currentIndexFriend -= 1;
+                    return null;
+                }
+                resultObject = {
+                    name: friends[currentIndexFriend].name,
+                    phone: collection[friends[currentIndexFriend].name].phone
+                };
+                return resultObject;
+            } else {
+                var newIndex = -1;
+                for (var i = 0; i < friends.length; i++) {
+                    if (friends[i].name === arguments[0]) {
+                        newIndex = i;
+                    }
+                }
+                if (newIndex == -1) {
+                    return null;
+                } else {
+                    currentIndexFriend = newIndex;
+                    resultObject = {
+                        name: friends[currentIndexFriend].name,
+                        phone: collection[friends[currentIndexFriend].name].phone
+                    };
+                    return resultObject;
+                }
+            }
+        },
 
-module.exports.get = function (collection, startPoint, depth) {
+        prev: function () {
+            var resultObject;
+            if (!arguments[0]) {
+                currentIndexFriend -= 1;
+                if (currentIndexFriend <= 0) {
+                    currentIndexFriend += 1;
+                    return null;
+                }
+                resultObject = {
+                    name: friends[currentIndexFriend].name,
+                    phone: collection[friends[currentIndexFriend].name].phone
+                };
+            } else {
+                var newIndex = -1;
+                for (var i = 0; i < friends.length; i++) {
+                    if (friends[i].name === arguments[0]) {
+                        newIndex = i;
+                    }
+                }
+                if (newIndex == -1) {
+                    return null;
+                } else {
+                    currentIndexFriend = newIndex;
+                    resultObject = {
+                        name: friends[currentIndexFriend].name,
+                        phone: collection[friends[currentIndexFriend].name].phone
+                    };
+                    return resultObject;
+                }
+            }
+        },
+
+        nextMale: function () {
+            for (var i = currentIndexFriend + 1; i < friends.length; i++) {
+                if (collection[friends[i].name].gender === 'Мужской') {
+                    currentIndexFriend = i;
+                    var resultObject = {
+                        name: friends[currentIndexFriend].name,
+                        phone: collection[friends[currentIndexFriend].name].phone
+                    };
+                    return resultObject;
+                }
+            }
+            return null;
+        },
+
+        prevMale: function () {
+            for (var i = currentIndexFriend - 1; i > 0; i--) {
+                if (collection[friends[i].name].gender === 'Мужской') {
+                    currentIndexFriend = i;
+                    var resultObject = {
+                        name: friends[currentIndexFriend].name,
+                        phone: collection[friends[currentIndexFriend].name].phone
+                    };
+                    return resultObject;
+                }
+            }
+            return null;
+        }
+    };
 };
+function initFriends(collection, nameStart, maxDepth) {
+    var result = [];
+    var queue = [];
+    var friends = [];
+    var usedNames = {};
+    usedNames[nameStart] = true;
+    queue.push({name: nameStart, dist: 0});
+    result.push({name: nameStart, dist: 0});
+    while (queue.length > 0) {
+        var nowFriend = queue.shift();
+        var friends = [];
+        friends = collection[nowFriend.name].friends.sort();
+        for (var i = 0; i < friends.length; i++) {
+            if (nowFriend.dist + 1 > maxDepth) {
+                usedNames[friends[i]] = true;
+            } else {
+                if (!usedNames[friends[i]]) {
+                    var nowFriendObject = {name: friends[i], dist: nowFriend.dist + 1};
+                    result.push(nowFriendObject);
+                    queue.push(nowFriendObject);
+                    usedNames[nowFriendObject.name] = true;
+                }
+            }
+        }
+    }
+    return result;
+}
