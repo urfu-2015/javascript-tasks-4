7844287e86879b1fa25f459e96b47eb60adc2760
Natalia Aksenova <dotokoto@gmail.com>
2015-10-26 23:02:40 +0500

    Add solution
    Пока без дополнительного задания, его позже сделаю

--- iterator.js
+++ iterator.js
@@ -1,4 +1,111 @@
 'use strict';
 
 module.exports.get = function (collection, startPoint, depth) {
+    return new Iterator(collection, startPoint, depth);
 };
+
+function Iterator(collection, startPoint, depth) {
+    if (depth === undefined || depth > collection.length) {
+        depth = Object.keys(collection).length;
+    }
+    var friends = getFriends(collection, startPoint, depth);
+    var currentPerson = startPoint;
+    var index = -1;
+    var friendsCount;
+    if (friends) {
+        friendsCount = friends.length;
+    } else {
+        friendsCount = 0;
+    }
+    this.next = function (name) {
+        if (name === undefined) {
+            if (currentPerson === undefined || index === friendsCount - 1) {
+                return null;
+            }
+            index++;
+            currentPerson = friends[index].name;
+            return friends[index];
+        }
+        if (collection[name] === undefined || index === friendsCount - 1) {
+            return null;
+        }
+        index++;
+        while (friends[index].name !== name && index < friendsCount) {
+            index++;
+        }
+        if (index === friendsCount - 1) {
+            return null;
+        }
+        currentPerson = friends[index].name;
+        return friends[index];
+    };
+    this.prev = function (name) {
+        if (name === undefined) {
+            if (currentPerson === undefined || index <= 0) {
+                return null;
+            }
+            index--;
+            currentPerson = friends[index].name;
+            return friends[index];
+        }
+        if (collection[name] === undefined || index === 0) {
+            return null;
+        }
+        index--;
+        while (friends[index].name !== name && index !== 0) {
+            index--;
+        }
+        if (index === 0) {
+            return null;
+        }
+        currentPerson = friends[index].name;
+        return friends[index];
+    };
+    this.nextMale = function () {
+        var friend = this.next();
+        while (friend !== null && collection[friend.name].gender !== 'Мужской') {
+            friend = this.next();
+        }
+        return friend;
+    };
+    this.prevMale = function () {
+        var friend = this.prev();
+        while (friend !== null && collection[friend.name].gender !== 'Мужской') {
+            friend = this.next();
+        }
+        return friend;
+    };
+}
+
+function getFriends(collection, startPoint, depth) {
+    var people = [];
+    if (collection[startPoint] === undefined || depth < 1) {
+        return null;
+    }
+    var queue = [];
+    queue.push(startPoint);
+    var used = [];
+    var d = [];
+    var p = [];
+    used[startPoint] = true;
+    p[startPoint] = -1;
+    while (depth > 0 && queue.length > 0) {
+        depth--;
+        var v = queue.shift();
+        var friends = collection[v].friends.sort();
+        for (var i = 0; i < friends.length; i++) {
+            var to = friends[i];
+            if (!used[to]) {
+                used[to] = true;
+                people.push({
+                    name: to,
+                    phone: collection[to].phone
+                });
+                queue.push(to);
+                d[to] = d[v] + 1;
+                p[to] = v;
+            }
+        }
+    }
+    return people;
+}
