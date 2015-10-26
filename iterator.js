2e83a0567297ee68fc6bb6eb56d504749af63ff3
NeSmogPridymatNick <IceDruid@yandex.ru>
2015-10-26 14:58:26 +0400

    add iterator.js

--- iterator.js
+++ iterator.js
@@ -1,4 +1,62 @@
 'use strict';
 
-module.exports.get = function (collection, startPoint, depth) {
-};
+module.exports.get = function(phoneBook, startPosition, depthFind) {
+    if (phoneBook[startPosition] === undefined) {
+        return {
+            next: null,
+            prev: null,
+        };
+    };
+    if (depthFind === undefined) {
+        depthFind = 0;
+    };
+    var friends = findFriends(phoneBook, startPosition, depthFind);
+    return {
+
+        curIndex : 0 ,
+        next : function() {
+            if (this.curIndex === friends.length)
+                return null
+            else{
+                this.curIndex++;
+                var resFriend = friends[this.curIndex]; 
+                return resFriend;
+            }
+        },
+        prev : function() {
+            if (this.curIndex === 0)
+                return null
+            else{
+                this.curIndex--; 
+                var resFriend = friends[this.curIndex];
+                return resFriend;
+            }
+        }
+    }
+}
+function findFriends(phoneBook, startPosition, depthFind) {
+    var queue = [[startPosition, 0]],
+        friendsNames = [startPosition],
+        friends = [],
+        depth = 0,
+        fname = '',
+        fFriends=[];
+    while (queue.length > 0 && depth < depthFind) {
+        var current = queue.shift();
+        fname = current[0];
+        depth = current[1];
+        friends.push({
+            name : fname,
+            phone : phoneBook[fname].phone
+        });
+        friendsNames.push(fname);
+        fFriends = phoneBook[fname].friends;
+        for(var i = 0; i < fFriends.length; i++){
+                if (friendsNames.indexOf(fFriends[i]) < 0) {
+                    queue.push([fFriends[i], depth + 1]);
+                };
+        };
+    };
+    return friends;
+}
+
