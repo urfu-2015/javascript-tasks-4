0da45186330d3f5134f09a72a837a9be1e4df721
v-samoylov <v-samoylov@users.noreply.github.com>
2015-10-31 18:27:04 +0500

    Update iterator.js

--- iterator.js
+++ iterator.js
@@ -1,122 +1,153 @@
 'use strict';
 
 module.exports.get = function (collection, startPoint, depth) {
+    var contactQueue = [];
     var contactName = startPoint;
-    var visited = new Set([contactName]);
     var contact = collection[contactName];
-    var queue;
-    if (contact) {
-        var friendList = contact.friends.sort();
-        var queue = [].concat(friendList);
-    }
-    var qc = -1; // queue counter
-    var handShakes = 0;
-    return {
-        next: function (name) {
-            if (name) {
-                qc = queue.indexOf(name) - 1;
+    contact.name = contactName;
+    contact.source = contact;
+    contact.depth = 0;
+    var visitedContacts = new Set([contact.name]);
+
+    var add2Queue = function (contactNames) {
+        return contactQueue.concat(contactNames.map(function (_contactName) {
+            var _contact = collection[_contactName];
+            if (visitedContacts.has(_contactName) || !_contact) {
+                return;
             }
-            qc++;
-            if (!contact || handShakes === depth) {
-                return null;
-            }
-            contactName = queue[qc];
-            if (!contactName) {
-                return null;
-            }
-            contact = collection[contactName];
-            var contactPhone = contact.phone;
-            var friendList = contact.friends.sort();
-            var json = {
-                name: contactName,
-                phone: contactPhone
-            };
-            queue = queue.concat(friendList.filter(function (entry) {
-                if (visited.has(entry)) {
-                    return false;
-                }
+            var newContact = Object.assign({}, _contact);
+            newContact.name = _contactName;
+            newContact.source = contact;
+            newContact.friends = newContact.friends.filter(function (_contactName) {
+                return _contactName !== contact.name;
+            });
+            newContact.depth = contact.depth + 1;
+            return newContact;
+        })).filter(function (_contact) {
+            return (typeof _contact !== 'undefined');
+        });
+    };
+
+    var isCollectionChanged = function () {
+        var _contact = contact;
+        do {
+            var contactIsDeleted = typeof collection[_contact.name] === 'undefined';
+            var sourceIsDeleted = typeof _contact.source === 'undefined';
+            if (contactIsDeleted || sourceIsDeleted) {
                 return true;
-            }));
-            visited.add(contactName);
-            handShakes++;
+            }
+            _contact = _contact.source;
+        }
+        while (_contact.name !== startPoint);
+        return false;
+    };
+
+    contactQueue = add2Queue(contact.friends.sort());
+    var qc = -1; // contact queue counter
+
+    return {
+        next: function () {
+            do {
+                qc++;
+                contact = contactQueue[qc];
+                if (!contact || contact.depth > depth) {
+                    return null;
+                }
+                if (isCollectionChanged()) {
+                    contactQueue.splice(qc, 1);
+                    qc--;
+                    contact = {};
+                }
+                else {
+                    var friendList = contact.friends.sort();
+                    contactQueue = add2Queue(friendList);
+                }
+                var contactNameGiven = typeof arguments[0] !== 'undefined';
+                var contactNameFound = contact.name === arguments[0];
+                var contactIsVisited = visitedContacts.has(contact.name) && !contactNameGiven;
+                var contactIsDeleted = contact === {};
+            }
+            while ((contactNameGiven && !contactNameFound) || contactIsVisited || contactIsDeleted);
+            var json = {
+                name: contact.name,
+                phone: contact.phone
+            };
+            visitedContacts.add(contact.name);
             return json;
         },
 
-        prev: function (name) {
-            if (name) {
-                qc = queue.indexOf(name) + 1;
+        prev: function () {
+            do {
+                qc--;
+                contact = contactQueue[qc];
+                if (!contact || contact.depth > depth) {
+                    return null;
+                }
+                if (isCollectionChanged()) {
+                    contactQueue.splice(qc, 1);
+                    contact = {};
+                }
+                var contactNameGiven = typeof arguments[0] !== 'undefined';
+                var contactNameFound = contact.name === arguments[0];
+                var contactIsDeleted = contact === {};
             }
-            qc--;
-            if (!contact || handShakes === depth) {
-                return null;
-            }
-            contactName = queue[qc];
-            if (!contactName) {
-                return null;
-            }
-            contact = collection[contactName];
-            var contactPhone = contact.phone;
+            while ((contactNameGiven && contactNameFound) || contactIsDeleted);
             var json = {
-                name: contactName,
-                phone: contactPhone
+                name: contact.name,
+                phone: contact.phone
             };
-            visited.add(contactName);
-            handShakes++;
+            visitedContacts.add(contact.name);
             return json;
         },
 
         nextMale: function () {
             do {
                 qc++;
-                if (!contact || handShakes === depth) {
+                contact = contactQueue[qc];
+                if (!contact || contact.depth > depth) {
                     return null;
                 }
-                contactName = queue[qc];
-                if (!contactName) {
-                    return null;
+                if (isCollectionChanged()) {
+                    contactQueue.splice(qc, 1);
+                    qc--;
+                    contact = {};
                 }
-                contact = collection[contactName];
-                var contactGender = contact.gender;
-                var friendList = contact.friends.sort();
-                queue = queue.concat(friendList.filter(function (entry) {
-                if (visited.has(entry)) {
-                    return false;
+                else {
+                    var friendList = contact.friends.sort();
+                    contactQueue = add2Queue(friendList);
                 }
-                return true;
-            }));
+                var contactIsVisited = visitedContacts.has(contact.name);
+                var contactIsDeleted = contact === {};
             }
-            while (contactGender != 'Мужской' || visited.has(contactName));
-            var contactPhone = contact.phone;
+            while ((contact.gender != 'Мужской') || contactIsVisited || contactIsDeleted);
             var json = {
-                name: contactName,
-                phone: contactPhone
+                name: contact.name,
+                phone: contact.phone
             };
-            visited.add(contactName);
-            handShakes++;
+            visitedContacts.add(contact.name);
             return json;
         },
 
         prevMale: function () {
             do {
                 qc--;
-                if (!contact || handShakes === depth) {
+                contact = contactQueue[qc];
+                if (!contact || contact.depth > depth) {
                     return null;
                 }
-                contactName = queue[qc];
-                if (!contactName) {
-                    return null;
+                if (isCollectionChanged()) {
+                    contactQueue.splice(qc, 1);
+                    contact = {};
                 }
-                contact = collection[contactName];
-                var contactGender = contact.gender;
+                var contactIsVisited = visitedContacts.has(contact.name);
+                var contactIsDeleted = contact === {};
             }
-            while (contactGender != 'Мужской');
-            var contactPhone = contact.phone;
+            while ((contact.gender != 'Мужской') || contactIsDeleted);
             var json = {
-                name: contactName,
-                phone: contactPhone
+                name: contact.name,
+                phone: contact.phone
             };
-            visited.add(contactName);
-            handShakes++;
+            visitedContacts.add(contact.name);
             return json;
         }
     };
