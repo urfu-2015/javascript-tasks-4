064fd4830eec1919d56a5fb88391f3e36044487f
Fedor <mydearfrodo@yandex.ru>
2015-10-31 18:56:50 +0500

    fixes2

--- iterator.js
+++ iterator.js
@@ -56,6 +56,7 @@ module.exports.get = function (collection, nameStart, maxDepth) {
                     name: friends[currentIndexFriend].name,
                     phone: collection[friends[currentIndexFriend].name].phone
                 };
+                return resultObject;
             } else {
                 var newIndex = -1;
                 for (var i = 0; i < friends.length; i++) {
