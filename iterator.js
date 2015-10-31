fe356b5735ab8a09f60fe9719f915baa16759485
slavavikharev <slavavicharev@gmail.com>
2015-10-31 19:56:29 +0500

    Made extra

--- iterator.js
+++ iterator.js
@@ -6,23 +6,24 @@ module.exports.get = function (collection, startPoint, depth) {
 
 function Iterator(collection, startPoint, depth) {
     this.collection = collection;
-    this.callList = !collection[startPoint] ?
-        [] : BFS(collection, depth, [startPoint], [startPoint], 0);
-    this.callList.splice(0, 1);
-    this.iterIndex = -1;
+    this.me = startPoint;
+    this.depth = depth;
     this.current = null;
+    this.updateCallList();
 }
 
 Iterator.prototype.next = function (name) {
     if (name) {
-        this.iterIndex = this.callList.indexOf(name) - 1;
+        this.jumpTo(name);
     }
+    this.updateCallList();
     this.iterIndex = Math.min(this.callList.length, this.iterIndex + 1);
     this.current = this.callList[this.iterIndex] || null;
     return this.JSONCurrent();
 };
 
 Iterator.prototype.prev = function () {
+    this.updateCallList();
     this.iterIndex = Math.max(-1, this.iterIndex - 1);
     this.current = this.callList[this.iterIndex] || null;
     return this.JSONCurrent();
@@ -30,8 +31,9 @@ Iterator.prototype.prev = function () {
 
 Iterator.prototype.nextMale = function (name) {
     if (name) {
-        this.iterIndex = this.callList.indexOf(name) - 1;
+        this.jumpTo(name);
     }
+    this.updateCallList();
     do {
         this.next();
     } while (this.current && this.collection[this.current].gender !== 'Мужской');
@@ -39,6 +41,7 @@ Iterator.prototype.nextMale = function (name) {
 };
 
 Iterator.prototype.prevMale = function () {
+    this.updateCallList();
     do {
         this.prev();
     } while (this.current && this.collection[this.current].gender !== 'Мужской');
@@ -55,12 +58,28 @@ Iterator.prototype.JSONCurrent = function () {
     });
 };
 
+Iterator.prototype.jumpTo = function (name) {
+    this.iterIndex = this.callList.indexOf(name) - 1;
+    this.current = this.callList[this.iterIndex] || null;
+};
+
+Iterator.prototype.updateCallList = function () {
+    if (!this.collection[this.me]) {
+        this.callList = [];
+        this.iterIndex = -1;
+        return;
+    }
+    this.callList = BFS(this.collection, this.depth, [this.me], [this.me], 0);
+    this.callList.splice(0, 1);
+    this.iterIndex = this.callList.indexOf(this.current);
+};
+
 function BFS(collection, depth, result, queue, i) {
     if (i === depth || !queue.length) {
         return result;
     }
     collection[queue.shift()].friends
-        .filter(friend => result.indexOf(friend) === -1)
+        .filter(friend => result.indexOf(friend) === -1 && collection[friend])
         .sort()
         .forEach(friend => {
             queue.push(friend);
