9c4e5f2eea43dce53d4e2a17bb68981c38eb9338
danmir <danmir@me.com>
2015-11-01 17:43:16 +0500

    + дом задание с удалением

--- iterator.js
+++ iterator.js
@@ -21,6 +21,9 @@ function doesUserExists(collection, userName) {
 function getFriends(collection, startPoint, usedFriends) {
     var startPointFriends = collection[startPoint]['friends'];
     startPointFriends = startPointFriends.sort();
+    startPointFriends = startPointFriends.filter(function(friend) {
+        return collection[friend];
+    });
     var startPointNewFriends = [];
     var startPointFriendsLen = startPointFriends.length;
     for (var f = 0; f < startPointFriendsLen; ++f) {
@@ -33,65 +36,118 @@ function getFriends(collection, startPoint, usedFriends) {
 
 module.exports.get = function (collection, startPoint, depth) {
     depth = depth || Object.keys(collection).length;
+    var collectionCopy = (JSON.parse(JSON.stringify(collection)));
     var didFinish = false;
     var currDepth = 0;
     var currIndex = 0;
     var currFriends = [startPoint];
     var newLevel = [];
     var usedFriends = {};
+    var toShow = {};
+    var lastShown = {};
     usedFriends[startPoint] = collection[startPoint];
-    return {
-        next: function (name) {
-            // Имя еще не использую
-            if (!doesUserExists(collection, startPoint)) {
+    var _next = function (){
+        // Если стартовой точки нет, то сразу закончили
+        if (!doesUserExists(collection, startPoint)) {
+            didFinish = true;
+            return null;
+        }
+        // Если уже закончили, то продолжаем выдавать null
+        if (didFinish) {
+            return null;
+        }
+        // Выдаем друзей до нужной глубины
+        if (currDepth < depth) {
+            var tmpFriends = getFriends(collection, currFriends[currIndex], usedFriends);
+            //Если после 1-ого уровня друзей пусто, то дальше только null
+            if (!currDepth && !tmpFriends.length) {
                 return null;
             }
-            if (didFinish) {
+            //Формируем следующий уровень, пока просматриваем друзей текущего
+            newLevel = newLevel.concat(tmpFriends);
+            //Отмечаем тех, кого уже посмотрели
+            tmpFriends.forEach(function (item) {
+                usedFriends[item] = collection[item];
+            });
+            currIndex++;
+            //Если на новом уровне никого нет, то мы дошли до конца
+            if (newLevel.length === 0) {
+                didFinish = true;
                 return null;
             }
-            // Выдаем друзей до нужной глубины
-            if (currDepth < depth) {
-                var tmpFriends = getFriends(collection, currFriends[currIndex], usedFriends);
-                //Если после 1-ого уровня друзей пусто, то дальше только null
-                if (!currDepth && !tmpFriends.length) {
-                    return null;
-                }
-                //Формируем следующий уровень, пока просматриваем друзей текущего
-                newLevel = newLevel.concat(tmpFriends);
-                //Отмечаем тех, кого уже посмотрели
-                tmpFriends.forEach(function (item) {
-                    usedFriends[item] = collection[item];
-                });
-                currIndex++;
-                //Если на новом уровне никого нет, то мы дошли до конца
-                if (newLevel.length === 0) {
-                    didFinish = true;
-                    return null;
-                }
-                //Если на данном уровне дошли до конца - спускаемся на след.
-                if (currIndex === currFriends.length) {
-                    currFriends = currFriends.concat(newLevel);
-                    newLevel = [];
-                    currDepth++;
-                }
-                //Готовим вывод, проверив, что не дошли до конца
-                if (collection[currFriends[currIndex]]) {
-                    var toShow = {};
-                    toShow['name'] = currFriends[currIndex];
-                    toShow['phone'] = collection[currFriends[currIndex]]['phone'];
-                    console.log(toShow);
-                    return toShow;
-                }
-                return null;
+            //Если на данном уровне дошли до конца - спускаемся на след.
+            if (currIndex === currFriends.length) {
+                currFriends = currFriends.concat(newLevel);
+                newLevel = [];
+                currDepth++;
+            }
+            //Готовим вывод, проверив, что не дошли до конца
+            if (collection[currFriends[currIndex]]) {
+                toShow = {};
+                toShow['name'] = currFriends[currIndex];
+                toShow['phone'] = collection[currFriends[currIndex]]['phone'];
+                return toShow;
             }
             return null;
+        }
+        return null;
+    };
+    var _recalculateFriends = function () {
+        // Попытаемся теперь от начала дойти до последнего выданного человека
+        didFinish = false;
+        currDepth = 0;
+        currIndex = 0;
+        currFriends = [startPoint];
+        newLevel = [];
+        usedFriends = {};
+        usedFriends[startPoint] = collection[startPoint];
+        var friend = _next();
+        // Либо связь потерялась совсем и дойти до последнего выданного
+        // не сможем, либо дойдем, пересчитав все по ходу
+        while (friend && lastShown !== friend) {
+            friend = _next();
+        }
+        // Если дойти не смогли, то обратно от этого человека,
+        // шаг будет уже нельзя сделать (связей с друзьями не осталось),
+        // но возможно, дальше есть кто-то.
+        // В этом случае сделаем его стартовой точкой,
+        // а дальше сведем к предыдущим случаям
+        if (!friend) {
+            startPoint = friend['name'];
+            didFinish = false;
+            currDepth = 0;
+            currIndex = 0;
+            currFriends = [startPoint];
+            newLevel = [];
+            usedFriends = {};
+            usedFriends[startPoint] = collection[startPoint];
+        }
+    };
+    var didCollectionChanged = function () {
+        return collection !== collectionCopy;
+    };
+    return {
+        next: function (name) {
+            // Если нам дали имя, просто дойдем до него
+            if (doesUserExists(collection, name)) {
+                var friend = _next();
+                while (friend && name !== friend['name']) {
+                    friend = _next();
+                }
+                lastShown = (JSON.parse(JSON.stringify(friend)));
+                return friend;
+            }
+            var tmpNext = _next();
+            lastShown = (JSON.parse(JSON.stringify(tmpNext)));
+            return tmpNext;
         },
         prev: function () {
             if (currIndex > 0) {
                 currIndex--;
-                var toShow = {};
+                toShow = {};
                 toShow['name'] = currFriends[currIndex];
                 toShow['phone'] = collection[currFriends[currIndex]]['phone'];
+                lastShown = (JSON.parse(JSON.stringify(toShow)));
                 return toShow;
             }
             return null;
@@ -104,6 +160,9 @@ module.exports.get = function (collection, startPoint, depth) {
             return nextMale;
         },
         prevMale: function () {
+            if (didCollectionChanged()) {
+                _recalculateFriends();
+            }
             var prevMale = this.prev();
             while (prevMale !== null && collection[prevMale['name']]['gender'] !== 'Мужской') {
                 prevMale = this.prev();
