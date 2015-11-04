2e819d8546f2b682460ea9e4b30937cd836cb4ef
davletova <davletova.a.g@gmail.com>
2015-11-04 23:08:55 +0500

    Update iterator.js

--- iterator.js
+++ iterator.js
@@ -6,13 +6,13 @@ module.exports.get = function (collection, startPoint, depth) {
 	if (!isNaN(startPoint) || !(collection.hasOwnProperty(startPoint))) {
 		var getNull = function() {
 			return null;
-			}
+			};
 		iter.next = getNull;
 		iter.nextMale = getNull;
 		iter.prevMale = getNull;
 		iter.prev = getNull;
 		return iter;
-		}
+		};
 	//количесвто друзей в книге изначально(надо знать на случай, если вдруг кого-то удалим)
 	var primordialLengthPhB = Object.keys(collection).length;
 	//список тех, кого мы еще не обошли
@@ -32,14 +32,14 @@ module.exports.get = function (collection, startPoint, depth) {
 		var cloneCollection = {};
 		for (person in collection) {
 			cloneCollection[person] = collection[person];
-		}
+		};
 		listFriends = cloneCollection[startPoint].friends;
 		delete cloneCollection[startPoint];
 		//здесь составляем список тех, кого будем обходить на следующем круге
 		var nextListFriends = [];
 		if (depth === undefined) {
 			depth = Number.MAX_SAFE_INTEGER;
-		}
+		};
 		//здесь учитываем добавили ли мы всех кого хотели(если прошлись по всем друзьям друзей)
 		var allAdd = 0;
 		for (var circle = 0; circle < depth; circle++) {
@@ -50,16 +50,16 @@ module.exports.get = function (collection, startPoint, depth) {
 					delete cloneCollection[listFriends[j]];
 				} else {
 					allAdd++;
-				}
-			}
+				};
+			};
 			if (allAdd === listFriends.length) {
 				break;
-			}
+			};
 			listFriends = nextListFriends;
 			nextListFriends = [];
 			allAdd = 0;
-		}
-	}
+		};
+	};
 	makeListFriends(collection, startPoint, depth);
 	var next = function(smbdOrProp) {
 		//если вдруг из фейсбука кого-то удалили
@@ -69,12 +69,12 @@ module.exports.get = function (collection, startPoint, depth) {
 			for (person in listOfPrevious) {
 				if (listOfNext.hasOwnProperty(person)) {
 					delete listOfNext[person];
-				}else {
+				} else {
 					delete listOfPrevious[person];
 					previous.pop();
-				}
-			}
-		}
+				};
+			};
+		};
 		if (listOfNext.hasOwnProperty(smbdOrProp)) {
 			for (person in listOfNext) {
 				listOfPrevious[String(person)] = listOfNext[person];
@@ -82,20 +82,20 @@ module.exports.get = function (collection, startPoint, depth) {
 				delete listOfNext[person];
 				if (person === smbdOrProp) {
 					return listOfPrevious[person];
-				} 
-			}
+				}; 
+			};
 			return null;
-		}
+		};
 		for (var person in listOfNext) {
 			listOfPrevious[String(person)] = listOfNext[person];
 			previous.push(person);
 			delete listOfNext[person];
 			if (smbdOrProp === undefined || listOfPrevious[person].gender === smbdOrProp) {
 				return person;;
-			} 		
-		}
+			}; 		
+		};
 		return null;
-	}
+	};
 	var prev = function(gender) {
 		//если вдруг из фейсбука кого-то удалили
 		if (!(primordialLengthPhB === Object.keys(collection).length)) {
@@ -110,16 +110,16 @@ module.exports.get = function (collection, startPoint, depth) {
 					var index = previous.indexOf(person);
 					if (index >= 0) {
 						previous.splice(index, 1);
-					}
-				}
-			}
+					};
+				};
+			};
 			previous.push(actPerson);
 			listOfPrevious[String(actPerson)] = actPerson;
-		}
+		};
 		//если предыдущих больше нет
 		if (previous.length < 2) {
 			return null;
-		} 
+		}; 
 		var person = previous.pop();
 		listOfNext[String(person)] = listOfPrevious[person];
 		delete listOfPrevious[person];
@@ -130,17 +130,17 @@ module.exports.get = function (collection, startPoint, depth) {
 			if (gender === undefined || listOfNext[person].gender === gender) {
 				previous.pop();
 				return person;
-			} 
+			}; 
 			previous.pop();
-		}
+		};
 		return null;
-	}
+	};
 	var nextMale = function(name) {
 		return next('Мужской');
-	}
+	};
 	var prevMale = function() {
 		return prev('Мужской');
-	}
+	};
 	iter.next = next;
 	iter.prev = prev;
 	iter.nextMale = nextMale;
