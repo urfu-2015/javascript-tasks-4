a5f4f04fbca4ce26797458eee870d1e8eb2c222a
v-samoylov <v-samoylov@users.noreply.github.com>
2015-11-08 03:34:56 +0500

    основное & доп

--- iterator.js
+++ iterator.js
@@ -1,131 +1,132 @@
 'use strict';
 
 module.exports.get = function (collection, startPoint, depth) {
-    var contactQueue = [];
-    var qc = -1; // contact queue counter
-    var visitedContacts = new Set();
-    var contactName = startPoint;
-    var contact = collection[contactName];
+    var collectionSize = Object.keys(collection).length;
+    var log = [];
 
-    var add2Queue = function (contactNames) {
-        return contactQueue.concat(contactNames.map(function (_contactName) {
-            var _contact = collection[_contactName];
-            if (visitedContacts.has(_contactName) || !_contact) {
-                return;
+    var collectionHandler = function () {
+        if (collectionSize !== Object.keys(collection).length) {
+            contactQueue = initQueue();
+            collectionSize = Object.keys(collection).length;
+            do {
+                var contact = collection[log.pop()];
             }
-            _contact.friends = _contact.friends.filter(function (_contactName) {
-                return _contactName !== contact.name;
-            });
-            var newContact = Object.assign({}, _contact);
-            newContact.name = _contactName;
-            newContact.source = contact;
-            newContact.depth = contact.depth + 1;
-            return newContact;
-        })).filter(function (_contact) {
-            return (typeof _contact !== 'undefined');
-        });
-    };
-
-    var isCollectionChanged = function () {
-        var _contact = contact;
-        do {
-            var isContactDeleted = typeof collection[_contact.name] === 'undefined';
-            var isSourceDeleted = typeof _contact.source === 'undefined';
-            if (isContactDeleted || isSourceDeleted) {
-                return true;
-            }
-            _contact = _contact.source;
+            while (!contact && log);
+            queueCounter = contactQueue.indexOf(contact);
         }
-        while (_contact.name !== startPoint);
-        return false;
     };
 
-    if (contact) {
+    var initQueue = function () {
+        var getFriends = function (contact) {
+            return contact.friends.sort().map(function (entry) {
+                contactName = entry;
+                contact = collection[contactName];
+                if (!contact) {
+                    return;
+                }
+                contact.name = contactName;
+                return contact;
+            }).filter(function (entry){
+                return typeof entry !== 'undefined';
+            });
+        };
+        var contactName = startPoint;
+        var contact = collection[contactName];
+        if (!contact) {
+            return [];
+        }
         contact.name = contactName;
-        contact.source = contact;
-        contact.depth = 0;
-        visitedContacts.add(contact.name);
-        contactQueue.push(contact);
-        qc++;
-        contactQueue = add2Queue(contact.friends.sort());
-    }
+        var contactQueue = [contact];
+        var depthLevel = getFriends(contact);
+        contactQueue = contactQueue.concat(depthLevel);
+        var handShake = 1;
+        do {
+            depthLevel = depthLevel.reduce(function (previousContact, currentContact) {
+                return previousContact.concat(function () {
+                    return getFriends(currentContact);
+                }());
+            }, []);
+            var areContactsFound = false;
+            contactQueue = contactQueue.concat(depthLevel.filter(function (entry) {
+                if (contactQueue.indexOf(entry) !== -1) {
+                    return false;
+                }
+                areContactsFound = true;
+                return true;
+            }));
+            if (!areContactsFound) {
+                break;
+            };
+            handShake++;
+        }
+        while (handShake !== depth);
+        return contactQueue;
+    };
+
+    var contactQueue = initQueue();
+    var queueCounter = 0;
 
     return {
         next: function () {
+            collectionHandler();
+            var isNameGiven = arguments[0] !== undefined;
             do {
-                qc++;
-                contact = contactQueue[qc];
-                if (!contact || contact.depth > depth) {
+                queueCounter++;
+                var contact = contactQueue[queueCounter];
+                if (!contact) {
                     return null;
                 }
-                if (isCollectionChanged()) {
-                    contactQueue.splice(qc, 1);
-                    qc--;
-                    contact = {};
-                } else {
-                    var friendList = contact.friends.sort();
-                    contactQueue = add2Queue(friendList);
-                }
-                var isContactNameGiven = typeof arguments[0] !== 'undefined';
-                var isContactFound = contact.name === arguments[0];
-                var isContactDeleted = contact === {};
+                var isNameFound = contact.name === arguments[0];
             }
-            while ((isContactNameGiven && !isContactFound) || isContactDeleted);
-            var json = {
-                name: contact.name,
-                phone: contact.phone
-            };
-            visitedContacts.add(contact.name);
+            while(isNameGiven && !isNameFound);
+            var json = {};
+            json.name = contact.name;
+            json.phone = contact.phone;
+            log.push(json.name);
             return json;
         },
-
         prev: function () {
+            collectionHandler();
+            var isNameGiven = arguments[0] !== undefined;
             do {
-                qc--;
-                contact = contactQueue[qc];
-                if (!contact || contact.depth > depth) {
+                queueCounter--;
+                var contact = contactQueue[queueCounter];
+                if (!contact) {
                     return null;
                 }
-                if (isCollectionChanged()) {
-                    contactQueue.splice(qc, 1);
-                    contact = {};
-                }
-                var isContactNameGiven = typeof arguments[0] !== 'undefined';
-                var isContactFound = contact.name === arguments[0];
-                var isContactDeleted = contact === {};
+                var isNameFound = contact.name === arguments[0];
             }
-            while ((isContactNameGiven && !isContactFound) || isContactDeleted);
-            var json = {
-                name: contact.name,
-                phone: contact.phone
-            };
-            visitedContacts.add(contact.name);
+            while(isNameGiven && !isNameFound);
+            var json = {};
+            json.name = contact.name;
+            json.phone = contact.phone;
+            log.push(json.name);
             return json;
         },
-
         nextMale: function () {
+            collectionHandler();
             do {
                 var json = this.next();
-                if (json) {
-                    contact = collection[json.name];
-                } else {
+                if (!json) {
                     return null;
                 }
+                var contact = collection[json.name];
             }
-            while (contact.gender != 'Мужской');
+            while (contact.gender !== 'Мужской');
+            log.push(json.name);
             return json;
         },
-
         prevMale: function () {
+            collectionHandler();
             do {
                 var json = this.prev();
-                if (json) {
-                    contact = collection[json.name];
-                } else {
+                if (!json) {
                     return null;
                 }
+                var contact = collection[json.name];
             }
-            while (contact.gender != 'Мужской');
+            while (contact.gender !== 'Мужской');
+            log.push(json.name);
             return json;
         }
     };
