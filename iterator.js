ae1b7765bc6b58992b50782fabbed6160b18186a
alexander.vakhitov <deadhunter007@live.ru>
2015-11-08 22:14:10 +0500

    Исправлены недочеты.

--- iterator.js
+++ iterator.js
@@ -2,7 +2,7 @@
 
 module.exports.get = function (collection, startPoint, depth) {
     if (depth === undefined) {
-        depth = Object.keys(collection).length;
+        depth = Infinity;
     } else if (depth < 0) {
         console.error('Ошибка! Неверное значение аргумента!');
         return;
@@ -15,11 +15,10 @@ function Iterator(collection, startPoint, depth) {
     this._startPoint = startPoint;
     this._depth = depth;
     this._currentIndex = 0;
-    this._friendList = this._makeFriendList(collection, startPoint, depth);
+    this._list = this._getFriends(collection, startPoint, depth);
 }
 
-Iterator.prototype._makeFriendList =
-    function _makeFriendList(collection, startPoint, depth) {
+Iterator.prototype._getFriends = function _getFriends(collection, startPoint, depth) {
     if (collection[startPoint] === undefined) {
         return null;
     }
@@ -33,14 +32,15 @@ Iterator.prototype._makeFriendList =
         var tempResult = [];
         var current = queue.pop();
         for (var i = 0; i < collection[current.name].friends.length; ++i) {
-            if (collection[collection[current.name].friends[i]] !== undefined &&
+            var name = collection[current.name].friends[i];
+            if (collection[name] !== undefined &&
                 current.depth < depth &&
-                resultList.indexOf(collection[current.name].friends[i]) < 0) {
+                resultList.indexOf(name) < 0) {
                 queue.unshift({
-                    name: collection[current.name].friends[i],
+                    name: name,
                     depth: current.depth + 1
                 });
-                tempResult.push(collection[current.name].friends[i]);
+                tempResult.push(name);
             }
         }
         /* Сортировка по алфавиту */
@@ -52,39 +52,40 @@ Iterator.prototype._makeFriendList =
 
 Iterator.prototype._updateFriendList = function _updateFriendList() {
     var currentName = null;
-    if (this._friendList !== null) {
-        currentName = this._friendList[this._currentIndex];
+    if (this._list !== null) {
+        currentName = this._list[this._currentIndex];
     }
-    this._friendList =
-        this._makeFriendList(this._collection, this._startPoint, this._depth);
-    if (this._friendList === null) {
+    this._list =
+        this._getFriends(this._collection, this._startPoint, this._depth);
+    if (this._list === null) {
         console.error('Ошибка! Удалили стартового друга!');
         return;
-    } else if (this._friendList.indexOf(currentName) < 0) {
+    }
+    if (this._list.indexOf(currentName) < 0) {
         console.error('Ошибка! Удалили текущего друга!');
         return;
     }
-    this._currentIndex = this._friendList.indexOf(currentName);
+    this._currentIndex = this._list.indexOf(currentName);
 };
 
 Iterator.prototype._get = function _get(order) {
     order = order === 'next' ? 1 : -1;
     if (this._currentIndex + order < 0 ||
-        this._currentIndex + order >= this._friendList.length) {
+        this._currentIndex + order >= this._list.length) {
         return null;
     }
     this._currentIndex += order;
-    var tempName = this._friendList[this._currentIndex];
-    var tempPhone = this._collection[tempName].phone;
+    var name = this._list[this._currentIndex];
+    var phone = this._collection[name].phone;
     return {
-        name: tempName,
-        phone: tempPhone
+        name: name,
+        phone: phone
     };
 };
 
 Iterator.prototype.prev = function prev(name) {
     this._updateFriendList();
-    if (this._friendList === null) {
+    if (this._list === null) {
         return null;
     }
     /* Если определено имя, то ищем его */
@@ -93,7 +94,7 @@ Iterator.prototype.prev = function prev(name) {
         do {
             resultInfo = this._get('prev');
         }
-        while (resultInfo != null && resultInfo.name != name);
+        while (resultInfo !== null && resultInfo.name !== name);
         return resultInfo;
     }
     return this._get('prev');
@@ -101,7 +102,7 @@ Iterator.prototype.prev = function prev(name) {
 
 Iterator.prototype.next = function next(name) {
     this._updateFriendList();
-    if (this._friendList === null) {
+    if (this._list === null) {
         return null;
     }
     /* Если определено имя, то ищем его */
@@ -122,7 +123,7 @@ Iterator.prototype.prevMale = function prevMale(name) {
         resultInfo = this.prev(name);
     }
     while (resultInfo != null &&
-            this._collection[resultInfo.name].gender != 'Мужской');
+    this._collection[resultInfo.name].gender != 'Мужской');
     return resultInfo;
 };
 
@@ -132,6 +133,6 @@ Iterator.prototype.nextMale = function nextMale(name) {
         resultInfo = this.next(name);
     }
     while (resultInfo != null &&
-            this._collection[resultInfo.name].gender != 'Мужской');
+    this._collection[resultInfo.name].gender != 'Мужской');
     return resultInfo;
 };
