21680a6000d9970e14d909a5100bc03a6b4ee1a5
Aspromist <Aspromist@gmail.com>
2015-11-08 15:11:38 +0500

    fixed js-task-4

--- iterator.js
+++ iterator.js
@@ -1,36 +1,41 @@
 'use strict';
 
 module.exports.get = function (collection, startPoint, depth) {
-    var iterator = {count: -1, collection: [], started: true};
+    var iterator = {count: 0, collection: [], started: true};
     var stackFriends = [];
-    var visited = [];
+    var visited = [startPoint];
 
     if (Object.keys(collection).indexOf(startPoint) == -1) {
         iterator.started = false;
     } else {
         stackFriends.push({name: startPoint, depth: 0});
 
-        while (visited.length < Object.keys(collection).length) {
+        while (stackFriends.length > 0 && visited.length < Object.keys(collection).length) {
             var current = stackFriends.shift();
-            if (current.depth >= depth) {
+
+            if (depth && current.depth > depth) {
                 break;
             }
 
-            if (current.name !== startPoint) {
-                var note = collection[current.name];
-                note.name = current.name;
-                iterator.collection.push(note);
-            }
-
-            visited.push(current.name);
+            var note = collection[current.name];
+            note.name = current.name;
+            iterator.collection.push(note);
             var currentFriends = collection[current.name].friends.sort();
             for (var i = 0; i < currentFriends.length; i++) {
-                if (visited.indexOf(currentFriends[i]) === -1) {
+                if (collection[currentFriends[i]] &&
+                    visited.indexOf(currentFriends[i]) === -1) {
                     stackFriends.push({name: currentFriends[i], depth: current.depth + 1});
+                    visited.push(currentFriends[i]);
                 }
             }
         }
     }
+
+    iterator.refresh = function (currentFriend) {
+        var newIterator = module.exports.get(collection, startPoint, depth);
+        newIterator.next(currentFriend);
+        return newIterator;
+    };
 
     iterator.next = function () {
         if (!this.started) {
@@ -41,7 +46,8 @@ module.exports.get = function (collection, startPoint, depth) {
 
         if (arguments[0]) {
             while (this.count < this.collection.length &&
-                this.collection[this.count].name !== arguments[0]) {
+                (this.collection[this.count].name !== arguments[0] &&
+                this.collection[this.count].gender !== arguments[0])) {
                 this.count++;
             }
         }
@@ -55,8 +61,8 @@ module.exports.get = function (collection, startPoint, depth) {
         }
 
         return {name: this.collection[this.count].name, phone: this.collection[this.count].phone};
-        // console.log("name:" + this.collection[this.count].name + ", " +
-        //     "phone:" + this.collection[this.count].phone);
+        //console.log("name:" + this.collection[this.count].name + ", " +
+        //    "phone:" + this.collection[this.count].phone);
     };
 
     iterator.prev = function () {
@@ -67,7 +73,8 @@ module.exports.get = function (collection, startPoint, depth) {
         this.count--;
 
         if (arguments[0]) {
-            while (this.count > 0 && this.collection[this.count].name !== arguments[0]) {
+            while (this.count > 0 && (this.collection[this.count].name !== arguments[0] &&
+                this.collection[this.count].gender !== arguments[0])) {
                 this.count--;
             }
         }
@@ -81,57 +88,16 @@ module.exports.get = function (collection, startPoint, depth) {
         }
 
         return {name: this.collection[this.count].name, phone: this.collection[this.count].phone};
-        // console.log("name:" + this.collection[this.count].name + ", " +
+        //console.log("name:" + this.collection[this.count].name + ", " +
         //     "phone:" + this.collection[this.count].phone);
     };
 
     iterator.nextMale = function () {
-        if (!this.started) {
-            return null;
-        }
-
-        this.count++;
-
-        while (this.count < this.collection.length &&
-            this.collection[this.count].gender !== 'Мужской') {
-            this.count++;
-        }
-
-        if (this.count > this.collection.length) {
-            this.count = this.collection.length;
-        }
-
-        if ((this.count < 0) || (this.count >= this.collection.length)) {
-            return null;
-        }
-
-        return {name: this.collection[this.count].name, phone: this.collection[this.count].phone};
-        // console.log("name:" + this.collection[this.count].name + ", " +
-        //     "phone:" + this.collection[this.count].phone);
+        return this.next('Мужской');
     };
 
     iterator.prevMale = function () {
-        if (!this.started) {
-            return null;
-        }
-
-        this.count--;
-
-        while (this.count > 0 && this.collection[this.count].gender !== 'Мужской') {
-            this.count--;
-        }
-
-        if (this.count < -1) {
-            this.count = -1;
-        }
-
-        if ((this.count < 0) || (this.count >= this.collection.length)) {
-            return null;
-        }
-
-        return {name: this.collection[this.count].name, phone: this.collection[this.count].phone};
-        // console.log("name:" + this.collection[this.count].name + ", " +
-        //     "phone:" + this.collection[this.count].phone);
+        return this.prev('Мужской');
     };
 
     return iterator;
