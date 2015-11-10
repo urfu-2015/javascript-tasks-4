975270dc060ebb8772037844820c8e57ea85290b
Vedmax <max.vedernikov@gmail.com>
2015-11-10 16:48:14 +0500

    fixed bag with unknown person + style fixes

--- iterator.js
+++ iterator.js
@@ -5,21 +5,21 @@ function bfs(collection, startPoint, depth) {
     var dict = {};
     var friendList = [];
     while (queue.length > 0) {
-        var s = queue.shift();
-        dict[s['name']] = 1;
-        friendList.push(s);
-        if (s['depth'] === depth) {
+        var current = queue.shift();
+        dict[current.name] = 1;
+        friendList.push(current);
+        if (current.depth === depth) {
             continue;
         }
-        if (!s['person']['friends']) {
+        if (!current.person.friends) {
             continue;
         }
-        s['person']['friends'] = s['person']['friends'].sort();
-        for (var i = 0; i < s['person']['friends'].length; i++) {
-            var newMemberName = s['person']['friends'][i];
+        current.person.friends = current.person.friends.sort();
+        for (var i = 0; i < current.person.friends.length; i++) {
+            var newMemberName = current.person.friends[i];
             if (dict[newMemberName] === undefined) {
                 queue.push({name: newMemberName,
-                    person: collection[newMemberName], depth: s['depth'] + 1});
+                    person: collection[newMemberName], depth: current.depth + 1});
                 dict[newMemberName] = 1;
             }
         }
@@ -39,38 +39,43 @@ module.exports.get = function (collection, startPoint, depth) {
     return {
         next: function () {
             if (arguments[0] !== undefined) {
+                var inList = false;
                 for (var i = 0; i < friendList.length; i++) {
-                    if (friendList[i]['name'] === arguments[0]) {
+                    if (friendList[i].name === arguments[0]) {
                         index = i - 1;
+                        inList = true;
                     }
+                }
+                if (!inList) {
+                    return null;
                 }
             }
             return index + 1 < friendList.length ?
-            { name: friendList[1 + index++]['name'],
-                phone: friendList[index]['person']['phone']} : null;
+            { name: friendList[1 + index++].name,
+                phone: friendList[index].person.phone} : null;
         },
         nextMale: function () {
             while (index + 1 < friendList.length) {
-                if (friendList[1 + index++]['person']['gender'] === 'Мужской') {
-                    return { name: friendList[index]['name'],
-                        phone: friendList[index]['person']['phone']};
+                if (friendList[1 + index++].person.gender === 'Мужской') {
+                    return { name: friendList[index].name,
+                        phone: friendList[index].person.phone};
                 }
             }
             return null;
         },
         prevMale: function () {
             while (index > 0) {
-                if (friendList[index-- - 1]['person']['gender'] === 'Мужской') {
-                    return { name: friendList[index]['name'],
-                        phone: friendList[index]['person']['phone']};
+                if (friendList[index-- - 1].person.gender === 'Мужской') {
+                    return { name: friendList[index].name,
+                        phone: friendList[index].person.phone};
                 }
             }
             return null;
         },
         prev: function () {
             return index > 0 ?
-            { name: friendList[index-- - 1]['name'],
-                phone: friendList[index]['person']['phone']} : null;
+            { name: friendList[index-- - 1].name,
+                phone: friendList[index].person.phone} : null;
         }
     };
 };
