7484c50c6afdb91d6a0a772401b7b0e12dd69cb5
davletova <davletova.a.g@gmail.com>
2015-11-11 23:52:24 +0500

    Davletova Aliya

--- iterator.js
+++ iterator.js
@@ -65,8 +65,8 @@ module.exports.get = function (collection, startPoint, depth) {
     };
     makeListFriends(collection, startPoint, depth);
     var changeName = function (name) {
-        return name.replace(/ё/gi, 'е');
-    };
+        return name.replace(/ё/gi, "е");
+    }
     var next = function (smbdOrProp) {
         //если вдруг из фейсбука кого-то удалили
         if (!(primordialLengthPhB === Object.keys(collection).length)) {
@@ -87,17 +87,19 @@ module.exports.get = function (collection, startPoint, depth) {
             listOfPrevious[String(person)] = listOfNext[person];
             previous.push(person);
             delete listOfNext[person];
-            if (smbdOrProp === undefined || listOfPrevious[person].gender === smbdOrProp ||
-                    person === smbdOrProp || changeName(person) === smbdOrProp) {
+            if (smbdOrProp === undefined || listOfPrevious[person].gender === smbdOrProp || person === smbdOrProp
+                    || changeName(person) === smbdOrProp) {
                 return listOfPrevious[person];
+                //return person;
             }
         }
         for (var person in listOfNext) {
             listOfPrevious[String(person)] = listOfNext[person];
             previous.push(person);
             delete listOfNext[person];
-            if (smbdOrProp === undefined || listOfPrevious[person].gender === smbdOrProp ||
-                    person === smbdOrProp || changeName(person) === smbdOrProp) {
+            if (smbdOrProp === undefined || listOfPrevious[person].gender === smbdOrProp || person === smbdOrProp
+                    || changeName(person) === smbdOrProp) {
+                //return person;
                 return listOfPrevious[person];
             }
         }
@@ -108,6 +110,9 @@ module.exports.get = function (collection, startPoint, depth) {
         if (!(primordialLengthPhB === Object.keys(collection).length)) {
             var actPerson = listOfPrevious[previous.pop()];
             makeListFriends(collection, startPoint, depth);
+            if (!listOfPrevious[actPerson]) {
+                return null;
+            }
             nextPer = [];
             listOfNext[startPoint] = listOfPrevious[startPoint];
             for (person in listOfPrevious) {
@@ -134,7 +139,10 @@ module.exports.get = function (collection, startPoint, depth) {
         delete listOfPrevious[person];
         for (var i = previous.length - 1; i >= 0; i--) {
             person = previous[i];
+            //return previous;
+            //return listOfNext;
             if (gender === undefined || listOfPrevious[person].gender === gender) {
+                //return person;
                 return listOfPrevious[person];
             }
             listOfNext[String(person)] = listOfPrevious[person];
@@ -149,9 +157,22 @@ module.exports.get = function (collection, startPoint, depth) {
     var prevMale = function () {
         return prev('Мужской');
     };
+    var getPrev = function () {
+        return listOfPrevious;
+    }
+    var getNext = function () {
+        return listOfNext;
+    }
+    var getNextPer = function () {
+        return nextPer;
+    }
     iter.next = next;
     iter.prev = prev;
     iter.nextMale = nextMale;
     iter.prevMale = prevMale;
+    iter.getPrev = getPrev;
+    iter.getNext = getNext;
+    iter.getNextPer = getNextPer;
+    iter.changeName = changeName;
     return iter;
 };
