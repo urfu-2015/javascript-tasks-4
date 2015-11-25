afcd0656933babd9b50c8d12d0ddff9500301459
Evgeny Petrov <jeysonep@gmail.com>
2015-11-25 21:41:46 +0500

    update

--- iterator.js
+++ iterator.js
@@ -1,94 +1,37 @@
 'use strict';
 
-module.exports.get = function (collection, startPoint, depth) {
-    var friends = getFriendsList(collection, startPoint, depth);
-    var currentIndex = 0;
-    var friendsLength = friends.length;
-
-    return {
-        next: function (friendName, gender) {
-            var nextFriend = null;
-            if (friendName) {
-                for (var i = 0; i < friendsLength; i++) {
-                    if (friends[i].name === friendName) {
-                        nextFriend = {name: friendName, phone: friends[i].phone};
-                        currentIndex = i + 1;
-                        break;
-                    }
-                }
-                return nextFriend;
-            }
-            if (friends.length > currentIndex) {
-                nextFriend = {name: friends[currentIndex].name, phone: friends[currentIndex].phone};
-                currentIndex += 1;
-            }
-            return nextFriend;
-        },
-
-        nextMale: function () {
-            var nextFriend = null;
-            for (var i = currentIndex; i < friendsLength; i++) {
-                if (friends[i].gender === 'Мужской') {
-                    nextFriend = {name: friends[i].name, phone: friends[i].phone};
-                    currentIndex = i + 1;
-                    break;
-                }
-            }
-            return nextFriend;
-        },
-
-        prev: function () {
-            var prevFriend = null;
-            currentIndex -= 2;
-            if (currentIndex >= 0 && currentIndex < friends.length) {
-                prevFriend = {name: friends[currentIndex].name, phone: friends[currentIndex].phone};
-            }
-            currentIndex += 1;
-            return prevFriend;
-        },
-
-        prevMale: function () {
-            var prevFriend = null;
-            currentIndex -= 2;
-            for (var i = currentIndex; i >= 0; i--) {
-                if (friends[i].gender === 'Мужской') {
-                    prevFriend = {name: friends[i].name, phone: friends[i].phone};
-                    currentIndex = i + 1;
-                    break;
-                }
-            }
-            return prevFriend;
-        }
-
-    };
-};
-
-
-function getFriendsList(collection, startPoint, depth) {
-    if (depth === 0 ){
-        return null;
-    }
-    depth = depth || Object.keys(collection).length;
+function getFriends(collection, startPoint, depth) {
+    depth = depth === 0 ? 0 : depth || Infinity;
     var friends = [];
     var visited = [];
     var depthLevel = 0;
     if (!collection[startPoint]) {
         return friends;
     }
+    friends.push({
+        name: startPoint,
+        phone: collection[startPoint].phone,
+        gender: collection[startPoint].gender
+    });
+
     var currentFriends = collection[startPoint].friends.sort();
     var added = [startPoint];
+
     function dfs(startPoint) {
-        visited.push(startPoint);
-        currentFriends = currentFriends.concat(collection[startPoint].friends.sort());
-        currentFriends.forEach(function (friend) {
-            if (added.indexOf(friend) === -1) {
-                friends.push({name: friend, phone: collection[friend].phone,
-                    gender: collection[friend].gender});
-                added.push(friend);
-            }
-        });
-        depthLevel += 1;
+        depthLevel++;
         if (depthLevel < depth) {
+            visited.push(startPoint);
+            currentFriends = currentFriends.concat(collection[startPoint].friends.sort());
+            currentFriends.forEach(function (friend) {
+                if (added.indexOf(friend) === -1) {
+                    friends.push({
+                        name: friend,
+                        phone: collection[friend].phone,
+                        gender: collection[friend].gender
+                    });
+                    added.push(friend);
+                }
+            });
             currentFriends.forEach(function (friend) {
                 if (visited.indexOf(friend) === -1) {
                     dfs(friend);
@@ -99,3 +42,84 @@ function getFriendsList(collection, startPoint, depth) {
     dfs(startPoint);
     return friends;
 }
+
+module.exports.get = function (collection, startPoint, depth) {
+    var friends = getFriends(collection, startPoint, depth);
+    var currentIndex = 1;
+    var nextFriend = null;
+    var prevFriend = null;
+    var friendsLength = friends.length;
+
+    return {
+        next: function (friendName) {
+            nextFriend = null;
+            if (friendName) {
+                for (var index = 0; index < friendsLength; index++) {
+                    if (friends[index].name === friendName) {
+                        nextFriend = {
+                            name: friendName,
+                            phone: friends[index].phone
+                        };
+                        currentIndex = ++index;
+                        break;
+                    }
+                }
+                return nextFriend;
+            }
+            if (0 <= currentIndex && currentIndex < friends.length) {
+                nextFriend = {
+                    name: friends[currentIndex].name,
+                    phone: friends[currentIndex].phone
+                };
+                currentIndex++;
+            }
+            return nextFriend;
+        },
+
+        nextMale: function () {
+            nextFriend = null;
+            if (0 <= currentIndex && currentIndex < friends.length) {
+                for (var index = currentIndex; index < friendsLength; index++) {
+                    if (friends[index].gender === 'Мужской') {
+                        nextFriend = {
+                            name: friends[index].name,
+                            phone: friends[index].phone
+                        };
+                        currentIndex = ++index;
+                        break;
+                    }
+                }
+            }
+            return nextFriend;
+        },
+
+        prev: function () {
+            prevFriend = null;
+            (nextFriend) ? currentIndex -= 2 : currentIndex--;
+            if (0 <= currentIndex && currentIndex < friends.length) {
+                prevFriend = {
+                    name: friends[currentIndex].name,
+                    phone: friends[currentIndex].phone
+                };
+            }
+            currentIndex++;
+            return prevFriend;
+        },
+
+        prevMale: function () {
+            prevFriend = null;
+            (nextFriend) ? currentIndex -= 2 : currentIndex--;
+            for (var index = currentIndex; index >= 0; index--) {
+                if (friends[index].gender === 'Мужской') {
+                    prevFriend = {
+                        name: friends[index].name,
+                        phone: friends[index].phone
+                    };
+                    currentIndex = (nextFriend) ? index++ : index;
+                    break;
+                }
+            }
+            return prevFriend;
+        }
+    };
+};
